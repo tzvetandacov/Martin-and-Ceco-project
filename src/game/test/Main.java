fe47693aad381bafccb8df0015359b3479d6afe9
martinyordanov90 <martinyordanov90@abv.bg>
2015-08-03 20:56:58 +0300

    Fixing bin

--- src/game/test/Main.java
+++ src/game/test/Main.java
@@ -1,10 +1,10 @@
 package game.test;
 
-public class HelloWorld {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Asd");
 	}
 
 }
